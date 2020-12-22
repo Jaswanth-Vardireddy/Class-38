5cc16df26f2c0dcf2dd5b477d9ca3725705906af
Jaswanth-Vardireddy <71151981+Jaswanth-Vardireddy@users.noreply.github.com>
2020-12-22 17:14:10 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var gameState=0;
+var playerCount=0;
+var database;
+var form,player,game;
+var allPlayers;
+var car1,car2, car3, car4;
+var cars=[];
+
+function setup()
+{
+createCanvas(displayWidth+200,displayHeight-30)
+database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+
+}
+
+function draw(){
+if(playerCount===4){
+game.updateState(1)
+}
+if(gameState===1){
+clear();
+game.play();
+}
+}
